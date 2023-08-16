781bf65a4499a79663879428335795942c4878a4
sksabbir2234 <sksabbir2234@gmail.com>
2023-08-16 16:22:06 +0600

    update

--- Baper Bank/banking.js
+++ Baper Bank/banking.js
@@ -25,24 +25,7 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
 })
 
 
-/* 
-1. add event handler with the withdraw button
-2. get the withdraw amount from the withdraw input field
-2-5. also make sure to convert the input into a number by using parseFloat
 
-3. Get previous withdraw total
-
-4. calculate total withdraw amount 
-4-5. set total withdraw amount
-
-5. get the previous balance total
-6. calculate new balance total
-6-5: set the new balance total
-
-7. clear the input field
-*/
-
-// step-1
 document.getElementById('btn-withdraw').addEventListener('click', function () {
     // step-2:
     const withdrawField = document.getElementById('withdraw-field');
