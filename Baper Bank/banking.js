fd36dd3597f19550999459bc577ad8652eee0ae6
sksabbir2234 <sksabbir2234@gmail.com>
2023-08-16 16:27:16 +0600

    wiithdraw update

--- Baper Bank/banking.js
+++ Baper Bank/banking.js
@@ -25,46 +25,31 @@ document.getElementById('btn-deposit').addEventListener('click', function() {
 
 
 
-
 document.getElementById('btn-withdraw').addEventListener('click', function () {
-    // step-2:
     const withdrawField = document.getElementById('withdraw-field');
-    const newWithDrawAmountString = withdrawField.value;
-    const newWithdrawAmount = parseFloat(newWithDrawAmountString);
-
-    // step-7
-    withdrawField.value = '';
+    const newWithdrawAmount = parseFloat(withdrawField.value);
 
     if (isNaN(newWithdrawAmount)) {
         alert('Please provide a valid number');
         return;
     }
 
-    // step-3
+    withdrawField.value = '';
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
-    const previousWithdrawTotalString = withdrawTotalElement.innerText;
-    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+    const previousWithdrawTotal = parseFloat(withdrawTotalElement.innerText);
 
-
-    // step-5
     const balanceTotalElement = document.getElementById('balance-total');
-    const previousBalanceTotalString = balanceTotalElement.innerText;
-    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
-
+    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
 
     if (newWithdrawAmount > previousBalanceTotal) {
         alert('Baap er bank e eto taka nai');
         return;
     }
 
-    // step-4
     const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+    withdrawTotalElement.innerText = currentWithdrawTotal.toFixed(2);
 
-
-    // step-6
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-    balanceTotalElement.innerText = newBalanceTotal;
-
-
-})
+    balanceTotalElement.innerText = newBalanceTotal.toFixed(2);
+});
